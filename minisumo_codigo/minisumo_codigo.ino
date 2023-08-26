88777b2a106aab8fded83e7c50fdf9027cf911a4
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2023-08-26 14:44:43 -0500

    iniciado en ArduinoIDE

--- minisumo_codigo/minisumo_codigo.ino
+++ minisumo_codigo/minisumo_codigo.ino
@@ -1,5 +1,6 @@
 #include <NewPing.h>
 //Esta media rara esta libreria porque no existe en mi laptop, lo que implica que lo agrego ariana?
+//curioso
 
 
 const int sensorDerechoPin = 13; // Cambiar al pin que corresponda
